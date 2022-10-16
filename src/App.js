36d0a24cdaef1e417b3cb2741607f0526c3b7cf4
Leonardo Murça <leo@leomurca.xyz>
2022-10-16 14:10:59 -0300

    Add responsive layout

--- src/App.js
+++ src/App.js
@@ -6,28 +6,30 @@ import githubActionsLogo from "./assets/github-actions-logo.png";
 
 function App() {
   return (
-    <div className="App">
-      <h1>rsync-deploy-react-app</h1>
-      <div className="Logotypes">
-        <img src={rsyncLogo} alt="Rsync Logo" />
-        <h1>+</h1>
-        <img src={reactLogo} alt="React Logo" />
-        <h1>+</h1>
-        <img src={githubActionsLogo} alt="Github Actions Logo" />
+    <>
+      <h1 style={{ textAlign: "center" }}>rsync-deploy-react-app</h1>
+      <div className="App">
+        <div className="Logotypes">
+          <img src={rsyncLogo} alt="Rsync Logo" />
+          <h1>+</h1>
+          <img src={reactLogo} alt="React Logo" />
+          <h1>+</h1>
+          <img src={githubActionsLogo} alt="Github Actions Logo" />
+        </div>
+        <p>
+          Web site created to illustrate how to deploy a react application using
+          rsync + Github Actions. <br /> See the full tutorial{" "}
+          <a
+            href="https://leomurca.xyz/posts/how-to-deploy-react-applications-using-github-actions-+-rsync/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            here
+          </a>
+          .
+        </p>
       </div>
-      <p>
-        Web site created to illustrate how to deploy a react application using
-        rsync + Github Actions. <br /> See the full tutorial{" "}
-        <a
-          href="https://leomurca.xyz/posts/how-to-deploy-react-applications-using-github-actions-+-rsync/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          here
-        </a>
-        .
-      </p>
-    </div>
+    </>
   );
 }
 
